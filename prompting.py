0e45af82621faa163deb6d31564789dc4372445b
Rachana-choudhary <146935416+Rachana-choudhary@users.noreply.github.com>
2023-10-27 02:20:50 +0530

    Update prompting.py

--- prompting.py
+++ prompting.py
@@ -3,21 +3,17 @@ import openai
 import toml
 import streamlit as st
 
-#secrets = toml.load("secrets.toml")
-#openai.api_key = secrets["openai_api_key"]
+secrets = toml.load("secrets.toml")
+openai.api_key = secrets["openai_api_key"]
 
-openai.api_key = st.secrets["openai_api_key"]
-
-
-def generate_risks(risk_description):
-    text = f"""Rewrite the following text "{risk_description}"  by correcting the grammatical errors
-     Please take some time to think and then provide only a complete rewritten text , I don't need any explanation."""
+def generate_description(loss_event, num_languages, language_category):
+    risk_titles = f"""As an expert in loss events, please translate the provided loss event description: {loss_event} into the following number of languages: {num_languages} into the following language category: {language_category}. Please ensure the translations are accurate and take the necessary time for precision. Use BOLD for the language name and standard font for the description."""
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": text}],
+        model="gpt-4",
+        messages=[{"role": "user", "content": risk_titles}],
         temperature=0.6,
-        max_tokens=3000,
+        max_tokens=7500,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
