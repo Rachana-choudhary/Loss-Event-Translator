23d8a5647041f8a632bec2706376ad7afbf8a349
Rachana-choudhary <146935416+Rachana-choudhary@users.noreply.github.com>
2023-10-27 02:36:12 +0530

    Update prompting.py

--- prompting.py
+++ prompting.py
@@ -3,8 +3,10 @@ import openai
 import toml
 import streamlit as st
 
-secrets = toml.load("secrets.toml")
-openai.api_key = secrets["openai_api_key"]
+#secrets = toml.load("secrets.toml")
+#openai.api_key = secrets["openai_api_key"]
+
+openai.api_key = st.secrets["openai_api_key"]
 
 def generate_description(loss_event, num_languages, language_category):
     risk_titles = f"""As an expert in loss events, please translate the provided loss event description: {loss_event} into the following number of languages: {num_languages} into the following language category: {language_category}. Please ensure the translations are accurate and take the necessary time for precision. Use BOLD for the language name and standard font for the description."""
